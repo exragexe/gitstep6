0340a012c6ea0c2443dfc1dcd91ce4e27269bfba
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 11:19:07 +0300

    lab 6 done

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+
+def truu(x):
+    try:
+        if x > 0:
+            return "true"
+        else:
+            return "false"
+
+    except Exception as ex:
+        print(ex)
+x=int(input("Enter numbers: "))
+print(truu(x))
