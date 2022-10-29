da5c64b7d9790457806dac24bbbc1479376ddfb9
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 11:09:00 +0300

    lab 3 done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+
+def proste(n):
+    try:
+        d = 2
+        while n % d != 0:
+            d += 1
+        return d == n
+    except Exception as ex:
+        print(ex)
+n = int(input("Enter number: "))
+print(proste(n))
