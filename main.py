e273e2214cb16f53e7ee0d739750e068c1e74ee2
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 11:18:15 +0300

    lab 5 done

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+
+def max(x,y):
+    try:
+        if x > y:
+            print(f"{x} Max")
+        else:
+            print(f"{y} Max")
+
+    except Exception as ex:
+        print(ex)
+x=int(input("Enter numbers: "))
+y=int(input())
+print(max(x,y))
+
+
+
