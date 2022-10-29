b230287193930faa02179f6191724914daec560f
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 10:47:59 +0300

    lab 2 done

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 
+
+def factorial(y):
+    try:
+        if y == 0 or y == 1:
+            return 1
+        else:
+            fa = 1
+            for i in range(1, y + 1):
+                fa = fa * i
+            return fa
+    except Exception as ex:
+        print(ex)
+
+
+def main():
+    x = int(input('x->'))
+    print(factorial(x))
+
+
+main()
+
