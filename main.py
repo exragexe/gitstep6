0b05606596044256135fea3ece29148a22aef852
YaroslavDisiak <yaroslavdisiak@gmail.com>
2022-10-29 09:46:33 +0300

    lab 1 done

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+def user(x,y):
+    try:
+        for i in range(x):
+            for j in range(y):
+                if i == 0 or i == x - 1:
+                    print("* ", end=" ")
+                else:
+                    if j == 0 or j == y - 1:
+                        print("* ", end=" ")
+                    else:
+                        print("  ", end=" ")
+            print()
+    except Exception as ex:
+        print(ex)
+
+
+
+
+user(3,5)
